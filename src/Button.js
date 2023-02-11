8ce1b1231ccb34d08a87b96582a3c277d474adf2
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-02-11 23:49:05 +0100

    Linkable Buttons

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,13 @@
 import "./style.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Button({text, image}) {
+function Button({text, image, link}) {
     return (
-        <div className="Button">
+        <Link to={link} className="Button">
             <img className="Icon" src={image} />
             <p className="Button_text">{text}</p>
-        </div>
+        </Link>
     )
 }
 
