4ac38928e01598d89b516b399d2a0cf02ca6ee40
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-02-11 23:48:50 +0100

    Create App.js and Contact.js

---
+++ src/routes/App.js
@@ -0,0 +1,23 @@
+import "../style.css";
+import Button from "../Button";
+import profile from "../images/me.jpg";
+import github from "../images/github.png";
+import contact from "../images/email.png"
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='Header'>
+        <img className="Profile" src={profile}/>
+        <h1>Mirel Korajac</h1>
+        <h3>Passionate Programmer</h3>
+      </div>
+      <div className="Button_Container">
+        <Button link="https://github.com/m1rels" text="Github" image={github}/>
+        <Button link="contact" text="Contact" image={contact}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/Contact.js
@@ -0,0 +1,14 @@
+import "../style.css";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Contact() {
+    return (
+        <div className="App">
+        <h1>Contact</h1>
+        <h3><Link to="mailto:mirelkorajac@gmail.com">mirelkorajac@gmail.com</Link></h3>
+        </div>
+    )
+}
+
+export default Contact;
