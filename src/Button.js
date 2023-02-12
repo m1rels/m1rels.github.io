c0858ed198a9562ec1e94b926a4b874125daa5b4
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-02-12 15:29:58 +0100

    Add alt prop to images

--- src/Button.js
+++ src/Button.js
@@ -2,10 +2,10 @@ import "./style.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Button({text, image, link}) {
+function Button({text, image, link, string}) {
     return (
         <Link to={link} className="Button">
-            <img className="Icon" src={image} />
+            <img className="Icon" src={image} alt={string} />
             <p className="Button_text">{text}</p>
         </Link>
     )
--- src/routes/App.js
+++ src/routes/App.js
@@ -8,13 +8,13 @@ function App() {
   return (
     <div className='App'>
       <div className='Header'>
-        <img className="Profile" src={profile}/>
+        <img className="Profile" src={profile} alt="Profile picture of Mirel Korajac"/>
         <h1>Mirel Korajac</h1>
         <h3>Passionate Programmer</h3>
       </div>
       <div className="Button_Container">
-        <Button link="https://github.com/m1rels" text="Github" image={github}/>
-        <Button link="contact" text="Contact" image={contact}/>
+        <Button link="https://github.com/m1rels" text="Github" image={github} string="Github Icon"/>
+        <Button link="contact" text="Contact" image={contact} string="Email Icon"/>
       </div>
     </div>
   );
