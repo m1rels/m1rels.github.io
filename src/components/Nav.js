a40662f7bf34ee5dd56d9c2c8752b74620602cad
Mirel Korajac <82174633+m1rels@users.noreply.github.com>
2023-03-27 14:37:11 +0200

    Building a LanguageSwitcher (Test)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,22 @@ import "../style.css";
 import React, {useState} from "react";
 import dollar from "../images/dollar.png";
 import { Link } from "react-router-dom";
+import Collapsible from "./Collapsible";
 
 function Nav() {
     
     const [isNavExpanded, setIsNavExpanded] = useState(false);
 
     return(
+        
         <nav className={isNavExpanded ? "collapsible--expanded nav" : "collapsible nav"}>
             <Link to="/"><img className="nav__logo" src={dollar} /></Link>
+            <Collapsible />
             <button className="nav__toggler" 
                     onClick={() => {
                         setIsNavExpanded(!isNavExpanded);
             }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-list" viewBox="0 0 16 16" className="toggler">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-list" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
             </button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,24 +7,60 @@ import Nav from "../components/Nav";
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import Collapsible from "../components/Collapsible";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
 
 
-export default function App() {
+export default function Home() {
+
+  const translations = {
+     en: {
+      header: 'Passionate Programmer',
+      buttonText: "Contact"
+    },
+     deu: {
+      header: 'Leidenschaftlicher Programmierer',
+      buttonText: "Kontakt"
+    },
+  }
+
+  const [isEnglish, setIsEnglish] = useState(true);
+
+  function LanguageSwitcher() {
+
+    const [isCollapsibleExpanded, setIsCollapsibleExpanded] = useState(false);
+
+    return (
+      <div className={isCollapsibleExpanded ? "collapsible--expanded Collapsible" : "collapsible Collapsible"}>
+            <div onClick={() => {
+                setIsCollapsibleExpanded(!isCollapsibleExpanded);
+            }}>
+                <p className="Collapsible__heading">{isEnglish ? "English" : "German"}</p>
+                <FontAwesomeIcon icon={faChevronRight} style={{color: "#ffffff"}} className="toggler" />
+            </div>
+            <div className="collapsible__content" onClick={() => {
+                setIsEnglish(!isEnglish)
+            }}>
+                <p className="Collapsible__selection">{isEnglish ? "German" : "English"}</p>
+            </div>
+        </div>
+    )
+  }
 
   return (
     <div>
-      <Nav />
       <div data-aos="zoom-in" className="App__section">
       <div className='Header'>
         <img className="Profile" src={profile} alt="Profile of Mirel Korajac"/>
         <h1>Mirel Korajac</h1>
-        <h2>Passionate Programmer</h2>
+        <h2>{isEnglish ? translations.en.header : translations.deu.header}</h2>
       </div>
       <div className="Button_Container">
         <Button link="https://github.com/m1rels" text="Github" image={github} string="Github Icon"/>
-        <Button link="contact" text="Contact" image={contact} string="Email Icon"/>
+        <Button link="contact" text={isEnglish ? translations.en.buttonText : translations.deu.buttonText} image={contact} string="Email Icon"/>
       </div>
+      <LanguageSwitcher />
     </div>
     </div>
   );
