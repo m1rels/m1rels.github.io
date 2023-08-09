fa3916a520d4aba43fad621921cb06f671d72613
Mirel Korajac <mirelkorajac@gmail.com>
2023-08-09 19:49:14 +0200

    Add Context for switching language.

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import Contact from "./routes/Contact";
 import Projects from "./routes/Projects";
 import Nav from "./components/Nav";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import { LanguageProvider } from "./LanguageContext";
 
 export default function App() {
     return (
-        <HashRouter>
-            <Nav/>
-            <Routes>
-                <Route 
-                path="/"
-                element={<Home/>}
-                />
-                <Route 
-                path="/contact"
-                element={<Contact/>}
-                />
-                <Route 
-                path="/about"
-                element={<About/>}
-                />
-                <Route 
-                path="/projects"
-                element={<Projects/>}
-                />
+        <LanguageProvider>
+            <HashRouter>
+                <Nav/>
+                <Routes>
+                    <Route 
+                    path="/"
+                    element={<Home/>}
+                    />
+                    <Route 
+                    path="/contact"
+                    element={<Contact/>}
+                    />
+                    <Route 
+                    path="/about"
+                    element={<About/>}
+                    />
+                    <Route 
+                    path="/projects"
+                    element={<Projects/>}
+                    />
                 </Routes>
-        </HashRouter>
+            </HashRouter>
+        </LanguageProvider>
     )
 }
